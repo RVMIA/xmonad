357b02b3b2f414574982db0afec6190db17f25e6
Ame <elliotdtx@gmail.com>
2023-04-16 22:39:51 -0500

    init

---
+++ xmobar/xmobar.hs
@@ -0,0 +1,19 @@
+Config { font         = "Terminus"
+       , border       = BottomBM 4
+       , borderColor  = "#DE5E5E"
+       , bgColor      = "black"
+       , fgColor      = "grey"
+       , position     = Top L 
+       , lowerOnStart = True
+       , commands     = [ Run Weather "KDFW" ["-t", "Richardson : <tempF>°", "-L", "50", "-H", "90", "--normal", "grey", "--high", "#de5e5e", "--low", "lightblue"] 36000
+                        , Run Network "enp10s0f3u1u2" [ "-L", "0", "-H", "150", "--normal", "grey", "--high", "#de5e5e"] 10
+                        , Run MultiCpu ["autosystem", "autobar"] 50
+                        , Run Memory ["-t","Mem: <usedratio>%"] 50
+                        , Run Date "%_A %D %r" "date" 10
+			, Run Alsa "default" "Master" [ "--template", "<volumestatus>", "--suffix", "True", "--", "--on", "" ]
+                        , Run XMonadLog
+                        ]
+       , sepChar      = "%"
+       , alignSep     = "}{"
+       , template     = "%XMonadLog% }{ %multicpu% | %memory% | %enp10s0f3u1u2% | %KDFW% | %alsa:default:Master% | <fc=#DE5E5E>%date%</fc> "
+       }
---
+++ xmonad.hs
@@ -0,0 +1,82 @@
+import XMonad
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Loggers
+
+
+import XMonad.ManageHook
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.ManageHelpers
+
+myTerm = "kitty"
+myModMask = mod4Mask
+myBorderWidth = 3
+myNormalBorderColor  = "#222222"
+myFocusedBorderColor = "#DE5e5e"
+
+
+main :: IO ()
+main = xmonad
+       . ewmhFullscreen
+       . ewmh
+       . withEasySB (statusBarProp "xmobar ~/.config/xmonad/xmobar/xmobar.hs" (pure myXmobarPP)) defToggleStrutsKey
+       $ myConfig 
+	
+myConfig = def
+	{ terminal = myTerm 
+	, modMask = myModMask 
+	, borderWidth = myBorderWidth
+	, normalBorderColor = myNormalBorderColor
+	, focusedBorderColor = myFocusedBorderColor
+	, manageHook = myManageHook
+	, layoutHook = myLayout
+	}
+	`additionalKeysP`
+	[ ("M-f", spawn "firefox-bin")
+	, ("M-S-l", spawn "slock")
+	, ("M-S-e", spawn "emacs")
+	]
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+	[ className =? "Gimp" --> doFloat
+	, isDialog	      --> doFloat ]
+
+myLayout = (tiled ||| Mirror tiled |||  Full)
+	where
+		tiled = Tall nmaster delta ratio
+		nmaster = 1
+		ratio = 1/2
+		delta = 3/100
+
+myXmobarPP :: PP
+myXmobarPP = def
+  { ppSep             = walred " | "
+  , ppTitleSanitize   = xmobarStrip
+  , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+  , ppHidden          = white . wrap " " ""
+  , ppHiddenNoWindows = lowWhite . wrap " " ""
+  , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+  , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+  , ppExtras          = [logTitles formatFocused formatUnfocused]
+  }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+  
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow, walred :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+    walred = xmobarColor "#de5e5e" ""
